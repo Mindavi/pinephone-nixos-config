9358c36c3267b674ff326134cb11082dd80e3a12
Rick van Schijndel <rol3517@gmail.com>
2021-08-04 20:24:16 +0200

    disable ca-derivations for now

--- pinephone-configuration.nix
+++ pinephone-configuration.nix
@@ -10,7 +10,7 @@
     package = pkgs.nixUnstable;
     useSandbox = true;
     extraOptions = ''
-      experimental-features = nix-command flakes ca-derivations
+      experimental-features = nix-command flakes
     '';
   };
 
