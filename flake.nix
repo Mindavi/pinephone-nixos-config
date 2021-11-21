791104106cda332234e703060250497eed9d7635
Rick van Schijndel <rol3517@gmail.com>
2021-11-21 21:41:39 +0100

    flake: add different inputs on cross build vs native build

--- flake.nix
+++ flake.nix
@@ -6,22 +6,30 @@
     #url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
+  inputs.nixpkgs-cross = {
+    url = "github:NixOS/nixpkgs/6ad93ecdbb115566d66b7a04b914e7ac3d780cd9";
+  };
+
   inputs.mobile-nixos = {
-    #url = "github:NixOS/mobile-nixos/master";
-    url = "github:betaboon/mobile-nixos/fix/pinephone-uboot-2021.10";
+    url = "github:NixOS/mobile-nixos/master";
     flake = false;
   };
 
-  outputs = { self, nixpkgs, mobile-nixos }: {
+  inputs.mobile-nixos-cross = {
+    url = "github:Mindavi/mobile-nixos/wip-pinephone";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, mobile-nixos, nixpkgs-cross, mobile-nixos-cross }: {
     nixosConfigurations.pinephone-nixos = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [ ./pinephone-configuration.nix ];
       specialArgs = { inherit mobile-nixos; };
     };
-    nixosConfigurations.pinephone-nixos-cross = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.pinephone-nixos-cross = nixpkgs-cross.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./pinephone-configuration-cross.nix ];
-      specialArgs = { inherit mobile-nixos; };
+      specialArgs = { mobile-nixos = mobile-nixos-cross; };
     };
   };
 }
