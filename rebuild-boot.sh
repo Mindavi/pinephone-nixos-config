59487b9c3ef7c6c20dbe6078c123b1215b6cd1e2
Rick van Schijndel <rol3517@gmail.com>
2021-11-21 12:20:39 +0100

    remove old rebuild scripts

--- rebuild-boot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo nixos-rebuild boot -I nixos-config=./pinephone-configuration.nix -I nixpkgs=./nixpkgs -I mobile-nixos=./mobile-nixos
-
--- rebuild-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-nixos-rebuild build -I nixos-config=./pinephone-configuration.nix -I nixpkgs=./nixpkgs -I mobile-nixos=./mobile-nixos
-
