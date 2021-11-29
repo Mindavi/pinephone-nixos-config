4ff0981c91a5351ea871571153be72045abe94c0
Rick van Schijndel <rol3517@gmail.com>
2021-11-29 21:23:40 +0100

    release.nix: add normal eval-config option
    The documentation could be re-enabled when using this method.
    Not sure why this is the case.

--- release.nix
+++ release.nix
@@ -17,10 +17,19 @@ let
     device = "pine64-pinephone";
   });
 
+  pp-config-normal-eval = (import <nixpkgs/nixos/lib/eval-config.nix> {
+    system = "x86_64-linux";
+    modules = [
+      ./pinephone-configuration-cross-official.nix
+      (import <mobile/lib/configuration.nix> { device = "pine64-pinephone"; })
+    ];
+  });
+
 in {
   official = pp-config-official;
   official-image = pp-config-official.config.mobile.outputs.default;
   fork = pp-config-fork;
   fork-image = pp-config-fork.config.mobile.outputs.default;
+  normal-image = pp-config-normal-eval.config.mobile.outputs.default;
 }
 
