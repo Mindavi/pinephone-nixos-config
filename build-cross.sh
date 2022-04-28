8a4ec2f559170e4ee2ac8245f3311b950d0aca60
Rick van Schijndel <rol3517@gmail.com>
2022-04-28 20:14:49 +0200

    build-cross.sh: remove NIXPKGS_ALLOW_UNFREE again after changing the allowUnfreePredicate

--- build-cross.sh
+++ build-cross.sh
@@ -2,6 +2,7 @@
 
 # Due to env var, impure must be enabled.
 # Unfree is required due to unfree firmware for the pinephone.
-export NIXPKGS_ALLOW_UNFREE=1
-nix build -L .#nixosConfigurations.pinephone-nixos-cross-fork.config.mobile.outputs.default --impure --keep-going
+#export NIXPKGS_ALLOW_UNFREE=1
+nix build -L .#nixosConfigurations.pinephone-nixos-cross-fork.config.mobile.outputs.default --keep-going
+#nix build -L .#nixosConfigurations.pinephone-nixos-cross-official.config.mobile.outputs.default --keep-going
 
