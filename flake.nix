02e8bc1c17b5c3bc1826989c888f2ba1ca9da277
Rick van Schijndel <rol3517@gmail.com>
2024-08-15 07:19:55 +0200

    pin nixpkgs to a slightly older version for now to prevent eg25-manager conflicts
    See the linked issue for some background.
    Long story short: eg25-manager was in mobile-nixos, got upstreamed but not removed yet from mobile-nixos.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "Pinephone NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Temporary for now: https://github.com/mobile-nixos/mobile-nixos/issues/734#issuecomment-2272582716
+    nixpkgs.url = "github:NixOS/nixpkgs/9f4128e00b0ae8ec65918efeba59db998750ead6";
 
     mobile-nixos.flake = false;
     mobile-nixos.url = "github:mobile-nixos/mobile-nixos/development";
